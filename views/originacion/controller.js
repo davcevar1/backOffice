2038f49971cd77124cc1eb16b840c74e899a24c5
David Cevallos <dcevallosarmijos@gmail.com>
2023-05-05 17:12:34 -0500

    item

--- views/originacion/controller.js
+++ views/originacion/controller.js
@@ -828,7 +828,7 @@ define(function(require) {
         };
         */
         $scope.removeShareholder = function (dataItem){
-          var index = $scope.shareholdersList.dataSource.findIndex((dataAux) => dataItem.id === dataAux.id);
+          var index = $scope.shareholdersList.dataSource((dataAux) => dataItem.id === dataAux.id);
           $scope.shareholdersList.dataSource.splice(index,1)
           console.log($scope.shareholdersList.dataSource.get(dataItem))
           };
